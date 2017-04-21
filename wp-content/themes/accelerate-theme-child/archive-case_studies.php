4f8b25ca2082c94228a9a37faaab8c19e5b91c51
Ken Green <kagreen@me.com>
2017-04-21 07:52:08 -0500

    added archive page for case studies but page still needs work

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Archive page for Case Studies
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) {
+				
+				the_post(); ?>
+
+
+				<article class="case-study">
+					<aside class="case-study-sidebar">
+						<h2 class="title"><?php echo the_title(); ?></h2>
+						<h5 class="services"><?php echo get_field("services"); ?></h5>
+						<h6 class="client">Client: <?php echo get_field("client"); ?></h6>
+						<p><?php the_excerpt(); ?></p>
+						<p><a href="<?php the_permalink(); ?>">View Project ></a></p>
+					</aside>
+					
+					<div class="case-study-images">
+						<?php 
+						$FULL_SIZE = "full";
+						$image1 = get_field("image_1");
+						$image2 = get_field("image_2");
+						$image3 = get_field("image_3");
+							
+						if($image1) { 
+							echo wp_get_attachment_image( $image1, $FULL_SIZE );
+						} ?>
+					</div>
+				</article>
+			
+			<?php } ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
