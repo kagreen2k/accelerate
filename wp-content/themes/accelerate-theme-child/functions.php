8937fd54374d45749f61ed779299628fdd2dda01
Ken Green <kagreen@me.com>
2017-04-27 15:00:04 -0500

    about page completed

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -26,17 +26,31 @@
  */
 
 function create_custom_post_types() {
+
 	register_post_type ( 'case_studies', array (
 			'labels' => array (
 					'name' => __ ( 'Case Studies' ),
-					'singular_name' => __ ( 'Case Study' ) 
+					'singular_name' => __ ( 'Case Study' )
 			),
 			'public' => true,
 			'has_archive' => true,
 			'rewrite' => array (
-					'slug' => 'case-studies' 
-			) 
+					'slug' => 'case-studies'
+			)
 	) );
+	
+	register_post_type ( 'services', array (
+			'labels' => array (
+					'name' => __ ( 'Services' ),
+					'singular_name' => __ ( 'Service' )
+			),
+			'public' => true,
+			'has_archive' => false,
+			'rewrite' => array (
+					'slug' => 'services'
+			)
+	) );
+	
 }
 add_action ( 'init', 'create_custom_post_types' );
 
