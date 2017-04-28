bf31139651ef9521c7ef631bf736036bdd03ef8a
Ken Green <kagreen@me.com>
2017-04-28 11:26:57 -0500

    added 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+<div class="error-404">
+    <div>
+    	<h1>Bi do</h1>
+    	<h2>(Please accept my apologies. The page you were looking for could not be found.)</h2>
+    </div>
+</div>
+
+<?php get_footer(); ?>
